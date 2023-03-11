992265fdc130529d18da9c33bff090ee58564594
OmerMunk <omer.munk@payboxapp.com>
2023-03-11 20:26:43 +0200

    update

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import {BACK_END_SERVER_URL} from "./App";
 
-export const BASE_URL = `${BACK_END_SERVER_URL}/api/`
+export const BASE_URL = `ec2-16-171-23-217.eu-north-1.compute.amazonaws.com:8000/api/`
 export const SIGN_UP_URL = 'register/'
 export const TOKEN_URL = 'token/'
