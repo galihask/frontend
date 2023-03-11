be6577b4ee5aa37355d1e337cab458b3e24194ed
OmerMunk <omer.munk@payboxapp.com>
2023-03-11 19:01:05 +0200

    updated front project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,40 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import {Navbar, Nav, Container} from 'react-bootstrap'
 import {Link, NavLink} from 'react-router-dom'
+import CART_LOGO from '../images/pngegg.png';
 
 
-function Header() {
-  return (
-    <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-        <Container>
-        <Navbar.Brand>Proshope</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-          {/* <Nav.Link href='/cart'><i className="fas fa-shopping-cart"></i>Cart</Nav.Link> */}
-          {/* <Nav.Link href='/login'><i className="fas fa-user"></i>Login</Nav.Link> */}
-          <NavLink to={'/cart'}>Cart</NavLink>
-        </Nav>
-        </Navbar.Collapse>
-        </Container>
-      </Navbar>
-        
-    </header>
-  )
+
+function Header(props) {
+    return (
+        <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand>Proshope</Navbar.Brand>
+                    <Navbar.Brand>{props.isLogged ? 'LOGGED' : 'NOT LOGGED'}</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='mr-auto'>
+                            {/* <Nav.Link href='/cart'><i className="fas fa-shopping-cart"></i>Cart</Nav.Link> */}
+                            {/* <Nav.Link href='/login'><i className="fas fa-user"></i>Login</Nav.Link> */}
+                            {props.isLogged ?
+                                <NavLink to={'/cart'}>
+                                    <div style={{display:'flex', flexDirection:'row'}}>
+                                        <div style={{fontSize:'20px', color:'white'}}>cart</div>
+                                        <img src={CART_LOGO} width={'30px'}/>
+                                    </div>
+                            </NavLink> : null}
+                            {!props.isLogged ? <NavLink to={'/signup'}>Sign up</NavLink> : null}
+                            {!props.isLogged ? <NavLink to={'/login'}>Login</NavLink> : null}
+                            {<NavLink to={'/products'}>
+                                <div style={{fontSize:'20px', color:'white'}}>products</div>
+                            </NavLink>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+import {BACK_END_SERVER_URL} from "./App";
+
+export const BASE_URL = `${BACK_END_SERVER_URL}/api/`
+export const SIGN_UP_URL = 'register/'
+export const TOKEN_URL = 'token/'
